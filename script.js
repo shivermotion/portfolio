44976da28acf36330166bcec8e5a6c28e862f18a
Jason Day <jason.day.blue@gmail.com>
2021-11-08 21:51:44 -0600

    fixed multiple divs

---
+++ script.js
@@ -0,0 +1,23 @@
+let mainDiv = document.getElementById("mainDiv");
+
+mainDiv.scrollIntoView({
+	behavior: "smooth",
+	block: "start",
+	inline: "nearest",
+});
+
+let footerDiv = document.getElementById("footerDiv");
+
+footerDiv.scrollIntoView({
+	behavior: "smooth",
+	block: "start",
+	inline: "nearest",
+});
+
+let returnHome = document.getElementById("home");
+
+returnHome.scrollIntoView({
+	behavior: "smooth",
+	block: "start",
+	inline: "nearest",
+});
